0ff02491d318f011d321f25eabc533360dac5342
Sciallalo <50951344+Sciallalo@users.noreply.github.com>
2021-12-14 11:06:47 +0100

    Update injector.c

--- blinky/injector.c
+++ blinky/injector.c
@@ -1,30 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/uio.h>
-#include <unistd.h>
+//#include <sys/uio.h>
+//#include <unistd.h>
 #include <errno.h>
-#include <err.h>
+//#include <err.h>
 #include <time.h>
 
 void inject(unsigned long start_address, unsigned char * buffer, int target_size);
 
-pid_t simulator_pid;
+//pid_t simulator_pid;
+
+typedef struct{
+    char* name;
+    int type;
+    unsigned long address;
+} structure;
 
 int main(){
     
     srand((unsigned)time(NULL));
-    
-    
     unsigned long a_variable_address;
-    
-    
+
     char *error;
     int err;
-    
     unsigned char target_buffer[168];
+
+    FILE *fp;
+    int i=0, num, *luogo, *tempo, id;
+
+    structure struct_array[114];
+
+
+    if((fp=fopen("structures.txt", "r"))==NULL) {
+        exit(1);
+    }
+
+    printf("Inserire quante iniezioni si vuole tentare:\n");
+    scanf("%d",&num);
+
+    luogo = (int*) malloc(num * sizeof(int));
+    tempo = (int*) malloc(num * sizeof(int));
+
+    printf("Le strutture che possono essere iniettate sono:\n");
+    while(!feof(fp)){
+        fscanf(fp,"%d %s %d %lu\n", &id, struct_array[id].name, &struct_array[id].type, &struct_array[id].address );
+        printf("%d ..... %s\n", id, struct_array[id].name);
+    }
+
+    fclose(fp);
+    printf("\n\n");
+
+    for(i=0; i<num; i++){
+        printf("Inserire l'ID del luogo %d' che si vuole iniettare:\n",i);
+        scanf("%d",&luogo[i]);
+
+        printf("Inserire il valore di tempo %d' della finestra di esecuzione in millisecondi:\n",i);
+        scanf("%d",&tempo[i]);
+    }
+
+    for(i=0; i<num; i++){
+        printf("Iniziezione %d nella struttura %s entro %d ms\n", i, struct_array[luogo[i]].name, tempo[i]);
+    }
+
+    /*
     printf("dimensione buffer: %d\n", sizeof(target_buffer));
-    
     //local and remote must be always declared as arrays, even with 1 element, to be used with xxxx[0]
     struct iovec local[1];
     struct iovec remote[1];
@@ -81,9 +121,12 @@ int main(){
         //simulator process
         printf("child");
         execl("./Demo", "Demo", (char*)NULL);
-    }    
+    }
+    */
+    free(luogo);
+    free(tempo);
 }
-
+/*
 void inject(unsigned long start_address, unsigned char * buffer, int target_size){
 int nwrite;
     int target_byte_number = rand() % target_size;
@@ -130,6 +173,6 @@ int nwrite;
 		else
     printf("Iniettato byte %d\n", target_byte_number);
 }
-
+*/
 
 
