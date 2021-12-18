52467f1e3cb87d0413651c9bb0ee07efd88b1fcd
Giuseppe Ruggeri <ruggerigiuseppe_work@hotmail.com>
2021-12-18 17:22:05 +0100

    removed uncorrect log_structs

--- FreeRTOS/FreeRTOS/Demo/Common/Minimal/EventGroupsDemo.c
+++ FreeRTOS/FreeRTOS/Demo/Common/Minimal/EventGroupsDemo.c
@@ -208,7 +208,7 @@ void vStartEventGroupTasks( void )
     log_struct("EventGroups_SyncTask2", TYPE_TASK_HANDLE, xSyncTask2);
 
     /* log the event group handles */
-    log_struct("EventGroups_EventGroup", TYPE_EVENT_GROUP_HANDLE, xEventGroup);
+    //log_struct("EventGroups_EventGroup", TYPE_EVENT_GROUP_HANDLE, xEventGroup);
     log_struct("EventGroups_EventGroupISR", TYPE_EVENT_GROUP_HANDLE, xISREventGroup);
 }
 /*-----------------------------------------------------------*/
